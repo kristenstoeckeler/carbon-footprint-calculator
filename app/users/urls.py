959a25ed24ca1fd7b9f5bdd095daf5242fcf0618
Kristen Stoeckeler <kstoeckeler@gmail.com>
2026-04-25 15:42:30 -0500

    changed home page to be footprints page. Added conditional so user specific section doesnt show if params are missing

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from users import views as users_views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', users_views.register, name='home'),
+    path('', RedirectView.as_view(url='/footprints/', permanent=False), name='home'),
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
